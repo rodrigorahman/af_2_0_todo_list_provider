88fe917b5a9012145b978201d4f717e63bb63d82
Rodrigo <rodrigorahman@gmail.com>
2021-07-13 18:05:47 -0300

    finalizando tela de cadastro

--- lib/app/modules/auth/login/login_page.dart
+++ lib/app/modules/auth/login/login_page.dart
@@ -103,7 +103,9 @@ class LoginPage extends StatelessWidget {
                               children: [
                                 Text('Não tem conta?'),
                                 TextButton(
-                                  onPressed: () {},
+                                  onPressed: () {
+                                    Navigator.of(context).pushNamed('/register');
+                                  },
                                   child: Text('Cadastre-se'),
                                 )
                               ],
