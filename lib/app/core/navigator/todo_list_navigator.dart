5846b19b4584641d892243d5c655c5c7a070121c
Rodrigo <rodrigorahman@gmail.com>
2021-07-20 09:24:05 -0300

    Navegacao sem contexto e home e splash screen

---
+++ lib/app/core/navigator/todo_list_navigator.dart
@@ -0,0 +1,10 @@
+
+import 'package:flutter/material.dart';
+
+class TodoListNavigator {
+  TodoListNavigator._();
+
+  static final navigatorKey = GlobalKey<NavigatorState>();
+  static NavigatorState? get to => navigatorKey.currentState;
+
+}
--- lib/app/repositories/user/user_repository_impl.dart
+++ lib/app/repositories/user/user_repository_impl.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/services.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:todo_list_provider/app/exception/auth_exception.dart';
 
 import './user_repository.dart';
@@ -46,10 +47,10 @@ class UserRepositoryImpl implements UserRepository {
       print(e);
       print(s);
       throw AuthException(message: e.message ?? 'Erro ao realizar login');
-    } on FirebaseAuthException catch(e, s) {
+    } on FirebaseAuthException catch (e, s) {
       print(e);
       print(s);
-      if(e.code == 'wrong-password') {
+      if (e.code == 'wrong-password') {
         throw AuthException(message: 'Login ou senha inválidos');
       }
       throw AuthException(message: e.message ?? 'Erro ao realizar login');
@@ -57,15 +58,18 @@ class UserRepositoryImpl implements UserRepository {
   }
 
   @override
-  Future<void> forgotPassword(String email) async{
+  Future<void> forgotPassword(String email) async {
     try {
-      final loginMethods = await _firebaseAuth.fetchSignInMethodsForEmail(email);
-      
-      if(loginMethods.contains('password')) {
+      final loginMethods =
+          await _firebaseAuth.fetchSignInMethodsForEmail(email);
+
+      if (loginMethods.contains('password')) {
         await _firebaseAuth.sendPasswordResetEmail(email: email);
-      } else if(loginMethods.contains('google')) {
-        throw AuthException(message: 'Cadastro realizado com o google, não pode ser restado a senha');
-      }else {
+      } else if (loginMethods.contains('google')) {
+        throw AuthException(
+            message:
+                'Cadastro realizado com o google, não pode ser restado a senha');
+      } else {
         throw AuthException(message: 'E-mail não cadastrado');
       }
     } on PlatformException catch (e, s) {
@@ -73,6 +77,48 @@ class UserRepositoryImpl implements UserRepository {
       print(s);
       throw AuthException(message: 'Erro ao restar senha');
     }
+  }
 
+  @override
+  Future<User?> googleLogin() async {
+    List<String>? loginMethods;
+    try {
+      final googleSignIn = GoogleSignIn();
+      final googleUser = await googleSignIn.signIn();
+      if (googleUser != null) {
+        loginMethods =
+            await _firebaseAuth.fetchSignInMethodsForEmail(googleUser.email);
+      
+        if (loginMethods.contains('password')) {
+          throw AuthException(
+              message:
+                  'Você utilizou o e-mail para cadastro TodoList, caso  tenha esquecido sua senha por favor clique no link esqueci minha senha');
+        } else {
+          final googleAuth = await googleUser.authentication;
+          final firebaseCredencial = GoogleAuthProvider.credential(
+            accessToken: googleAuth.accessToken, idToken: googleAuth.idToken
+          );
+          var userCredencial = await _firebaseAuth.signInWithCredential(firebaseCredencial);
+          return userCredencial.user;
+        }
+      }
+    } on FirebaseAuthException catch (e, s) {
+      print(e);
+      print(s);
+      if(e.code == 'account-exists-with-different-credential') {
+        throw AuthException(message: '''
+          Login inválido você se registrou no TodoList com os seguintes provedores:
+            ${loginMethods?.join(',')}
+        ''');
+      } else {
+        throw AuthException(message: 'Erro ao realizar login');
+      }
+    }
+  }
+
+  @override
+  Future<void> googleLogout() async {
+    await GoogleSignIn().signOut();
+    _firebaseAuth.signOut();
   }
 }
